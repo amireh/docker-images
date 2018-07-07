76e4d3c1671c8e84ef1ff55d1c5c5d9bb9c4ab40
Ahmad Amireh <ahmad@amireh.net>
2018-07-08 01:58:04 +0300

    fix docker.sock test

--- ansible/test/mimic/groups_test.sh
+++ ansible/test/mimic/groups_test.sh
@@ -6,7 +6,7 @@ up() {
     b : docker_run_as "500" "10 15 20 25" mimic id -g
 
   assert.eql "it creates aux groups" \
-    a : "10 15 20 25" \
+    a : "10 15 20 25 $(stat -c '%g' /var/run/docker.sock)" \
     b : docker_run_as "500" "10 15 20 25" mimic id -G
 
   assert.eql "it works even if given 0:0 (root:root) for uid/gid" \
