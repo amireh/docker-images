a1dac1bcb302c3bb9765923f27b7499a92e6a53e
Ahmad Amireh <ahmad@amireh.net>
2018-10-10 08:45:30 -0600

    kong-dev: [ci] pass $KONG_VERSION as "version" to ansible

--- kong-dev/scripts/ci.sh
+++ kong-dev/scripts/ci.sh
@@ -1,3 +1,33 @@
 #!/usr/bin/env bash
 
-cd ci && medusa ansible-playbook ./playbook.yml "$@"
+P="$(basename "${0}")"
+
+if [[ ! -d ci ]]; then
+  echo "$P: must be run from the kong-dev directory" 1>&2
+  exit 1
+fi
+
+if [[ -z $KONG_VERSION ]]; then
+  {
+    echo "$P: missing KONG_VERSION environment variable"
+    echo "$P: usage example:"
+    echo "$P:"
+    echo "$P:     KONG_VERSION=0.12.3 ci.sh"
+  } 1>&2
+
+  exit 1
+fi
+
+type -p medusa 1>/dev/null || {
+  {
+    echo "$P: \"medusa\" is required to run the tests."
+    echo "$P: Refer to the installation notes in the following link"
+    echo "$P: and try again:"
+    echo "$P:"
+    echo "$P:     https://github.com/amireh/medusa#installation"
+  } 1>&2
+
+  exit 1
+}
+
+cd ci && medusa ansible-playbook ./playbook.yml -e version="$KONG_VERSION" "$@"
